5f6978c551ad0cff0406fa49b68a00e538654f38
lafaillejeremiah7-droid <lafaillejeremiah7@gmail.com>
2026-08-30 00:08:37 -0500

    db: export Deal Email and Call models

--- dashboard/models/__init__.py
+++ dashboard/models/__init__.py
@@ -1,8 +1,14 @@
+from .deal import Deal
 from .lead import Lead, PipelineState
 from .operator import Operator
+from .outreach import Call, CallOutcome, Email
 from .suppression import OutreachSuppression, SuppressionReason
 
 __all__ = [
+    "Call",
+    "CallOutcome",
+    "Deal",
+    "Email",
     "Lead",
     "Operator",
     "OutreachSuppression",
