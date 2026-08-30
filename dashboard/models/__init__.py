05079b6082a79aa82cb8b095b7c7c8adaa2cece7
lafaillejeremiah7-droid <lafaillejeremiah7@gmail.com>
2026-08-30 00:01:50 -0500

    db: export persistent Lead model

--- dashboard/models/__init__.py
+++ dashboard/models/__init__.py
@@ -1,4 +1,11 @@
+from .lead import Lead, PipelineState
 from .operator import Operator
 from .suppression import OutreachSuppression, SuppressionReason
 
-__all__ = ["Operator", "OutreachSuppression", "SuppressionReason"]
+__all__ = [
+    "Lead",
+    "Operator",
+    "OutreachSuppression",
+    "PipelineState",
+    "SuppressionReason",
+]
