0317687d5c38cf67435b2761cc72692696f6bba7
lafaillejeremiah7-droid <lafaillejeremiah7@gmail.com>
2026-08-29 23:22:16 -0500

    chore: export Scout and Researcher handoff contracts

--- dashboard/services/__init__.py
+++ dashboard/services/__init__.py
@@ -1,17 +1,27 @@
 from .closer import Closer, CloserDecision, ReplyCategory
 from .company import SevenEmployeeCompany
-from .discovery_handoff import DiscoveryHandoff, apply_discovery_handoff, verify_discovery_handoff
+from .discovery_handoff import (
+    ResearchHandoff,
+    ScoutHandoff,
+    apply_research_handoff,
+    apply_scout_handoff,
+    verify_research_handoff,
+    verify_scout_handoff,
+)
 from .six_employee_pipeline import Lead, PipelineResult, SixEmployeePipeline
 
 __all__ = [
     "Closer",
     "CloserDecision",
-    "DiscoveryHandoff",
     "Lead",
     "PipelineResult",
     "ReplyCategory",
+    "ResearchHandoff",
+    "ScoutHandoff",
     "SevenEmployeeCompany",
     "SixEmployeePipeline",
-    "apply_discovery_handoff",
-    "verify_discovery_handoff",
+    "apply_research_handoff",
+    "apply_scout_handoff",
+    "verify_research_handoff",
+    "verify_scout_handoff",
 ]
