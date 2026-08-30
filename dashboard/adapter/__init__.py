fd2005c9e96f7911e5ca790fdcfa683b79123dea
lafaillejeremiah7-droid <lafaillejeremiah7@gmail.com>
2026-08-29 23:29:09 -0500

    chore: export Gmail delivery adapter

--- dashboard/adapter/__init__.py
+++ dashboard/adapter/__init__.py
@@ -1,10 +1,20 @@
+from .gmail_delivery import (
+    DeliveryReceipt,
+    GmailDeliveryClient,
+    GmailDeliveryError,
+    build_raw_message,
+)
 from .google_places import GooglePlacesTextSearchClient, PlaceCandidate
 from .website_research import FetchedPage, WebsiteResearchClient, WebsiteResearchError
 
 __all__ = [
+    "DeliveryReceipt",
     "FetchedPage",
+    "GmailDeliveryClient",
+    "GmailDeliveryError",
     "GooglePlacesTextSearchClient",
     "PlaceCandidate",
     "WebsiteResearchClient",
     "WebsiteResearchError",
+    "build_raw_message",
 ]
