a1d422d07d416313b14cfb24386cb2b2e1ff054f
lafaillejeremiah7-droid <lafaillejeremiah7@gmail.com>
2026-08-29 20:08:09 -0500

    feat: export seven-employee company services

---
+++ dashboard/services/__init__.py
@@ -0,0 +1,13 @@
+from .closer import Closer, CloserDecision, ReplyCategory
+from .company import SevenEmployeeCompany
+from .six_employee_pipeline import Lead, PipelineResult, SixEmployeePipeline
+
+__all__ = [
+    "Closer",
+    "CloserDecision",
+    "Lead",
+    "PipelineResult",
+    "ReplyCategory",
+    "SevenEmployeeCompany",
+    "SixEmployeePipeline",
+]
