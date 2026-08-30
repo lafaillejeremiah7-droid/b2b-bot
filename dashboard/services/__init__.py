bf65455098a5721d502772fc1b4ae98a98bcd20f
lafaillejeremiah7-droid <lafaillejeremiah7@gmail.com>
2026-08-29 23:20:29 -0500

    chore: export discovery handoff contract

--- dashboard/services/__init__.py
+++ dashboard/services/__init__.py
@@ -1,13 +1,17 @@
 from .closer import Closer, CloserDecision, ReplyCategory
 from .company import SevenEmployeeCompany
+from .discovery_handoff import DiscoveryHandoff, apply_discovery_handoff, verify_discovery_handoff
 from .six_employee_pipeline import Lead, PipelineResult, SixEmployeePipeline
 
 __all__ = [
     "Closer",
     "CloserDecision",
+    "DiscoveryHandoff",
     "Lead",
     "PipelineResult",
     "ReplyCategory",
     "SevenEmployeeCompany",
     "SixEmployeePipeline",
+    "apply_discovery_handoff",
+    "verify_discovery_handoff",
 ]
