6794e3d9f36e692d694f6d0ba5973f74f84c442a
lafaillejeremiah7-droid <lafaillejeremiah7@gmail.com>
2026-08-30 15:46:50 -0500

    db: export core Task 2.3 models

--- dashboard/models/__init__.py
+++ dashboard/models/__init__.py
@@ -2,16 +2,38 @@ from .deal import Deal
 from .lead import Lead, PipelineState
 from .operator import Operator
 from .outreach import Call, CallOutcome, Email
+from .records import (
+    Contact,
+    Invoice,
+    OutreachChannel,
+    OutreachRequest,
+    OutreachRequestStatus,
+    Payment,
+    ReleaseAuthorization,
+    SitePage,
+    SiteProject,
+    SiteReviewState,
+)
 from .suppression import OutreachSuppression, SuppressionReason
 
 __all__ = [
     "Call",
     "CallOutcome",
+    "Contact",
     "Deal",
     "Email",
+    "Invoice",
     "Lead",
     "Operator",
+    "OutreachChannel",
+    "OutreachRequest",
+    "OutreachRequestStatus",
     "OutreachSuppression",
+    "Payment",
     "PipelineState",
+    "ReleaseAuthorization",
+    "SitePage",
+    "SiteProject",
+    "SiteReviewState",
     "SuppressionReason",
 ]
